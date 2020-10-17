451f6cd004366043f77c2c6026bd773b5ee17089
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2020-10-17 14:12:26 +0530

    requsting items functionality

--- technician/request.php
+++ technician/request.php
@@ -1,3 +1,43 @@
+<?php 
+
+require_once __DIR__ . '/../classes/ItemRequest.php';
+require_once __DIR__ . '/../classes/Inventory.php';
+
+$inv = new Inventory();
+$item_names = $inv->getItemNames();
+$item_names= $item_names->fetch_all();
+
+
+
+// create new requst when button pressed
+if (isset($_POST["addrequest"]) ) {
+    $request_items = array(); // item eke id eka ekka quantity eka me array ekata dagannawa
+
+    foreach ($item_names as $item){
+        //for collect used items quantities
+        $item_name = $item[0]."_u";
+        $quantity = $_POST["$item_name"];
+
+        if ($quantity!=0 && $quantity!=null) {
+
+            $request_item = array($item[0], $quantity);
+            $request_items[] = $request_item;
+        }
+        
+    }
+
+    if (!empty($request_items)) {
+        $itemrequest = new ItemRequest();
+        // danata created_user_id eka 1 authentication nathi nisa
+        $itemrequest->CreateItemRequest(1,$request_items );
+
+        echo ("<script>alert('repair completed succesfully') </script>");
+        header("location: ./index.php");
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,15 +58,15 @@
         <ul>
             <li class="nav-logo"><span class="nav-link" href="#"><i class="fas fa-lightbulb"></i><span class="link-text"
                         style="margin-left: 5px;">LiFix</span></span></li>
-            <li class="nav-item"><a class="nav-link " href="./index.html"><i class="fas fa-home"></i><span
+            <li class="nav-item"><a class="nav-link " href="./index.php"><i class="fas fa-home"></i><span
                         class="link-text">Home</span> </a></li>
             <li class="nav-item"><a class="nav-link " href="./map.html"><i class="fas fa-map"></i><span
                         class="link-text">ViewMap</span> </a></li>
-            <li class="nav-item"><a class="nav-link active" href="./request.html"><i
+            <li class="nav-item"><a class="nav-link active" href="./request.php"><i
                         class="fas fa-plus-square"></i><span class="link-text ">Request</span></a></li>
-            <li class="nav-item"><a class="nav-link" href="./EmgRepair.html"><i class="fas fa-exclamation-circle"></i><span
+            <li class="nav-item"><a class="nav-link" href="./EmgRepair.php"><i class="fas fa-exclamation-circle"></i><span
                         class="link-text">EmgRepair</span></a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-shower"></i><span
+            <li class="nav-item"><a class="nav-link" href="./lamppost.php"><i class="fas fa-shower"></i><span
                         class="link-text">Lamppost</span></a></li>
 
         </ul>
@@ -40,39 +80,23 @@
         <div class="con">
 
 
-            <form>
+            <form method="POST" action="request.php">
                 <h2>Add Item Request</h2>
 
-                <div class="collapsible">Bulbs</div>
-                <div class="content">
-                    <input class="field" type="text" placeholder="Enter Amount" name="bulb" id="">
-                </div>
-                <div class="collapsible">Sunboxes</div>
-                <div class="content">
-                    <input class="field" type="text" placeholder="Enter Amount" name="bulb" id="">
-                </div>
-                <div class="collapsible">Wires</div>
-                <div class="content">
-                    <input class="field" type="text" placeholder="Enter Amount" name="bulb" id="">
-                </div>
-                <div class="collapsible">Switches</div>
-                <div class="content">
-                    <input class="field" type="text" placeholder="Enter Amount" name="bulb" id="">
-                </div>
-                <div class="collapsible">Holderes</div>
-                <div class="content">
-                    <input class="field" type="text" placeholder="Enter Amount" name="bulb" id="">
-                </div>
-                <div class="collapsible">Screw holder</div>
-                <div class="content">
-                    <input class="field" type="text" placeholder="Enter Amount" name="bulb" id="">
-                </div>
-                <div class="collapsible">3 Pin holder</div>
-                <div class="content">
-                    <input class="field" type="text" placeholder="Enter Amount" name="bulb" id="">
-                </div>
 
-                <button type="submit" id="" class="btn">ADD REQUEST</button>
+                <?php 
+                foreach ($item_names as $item):
+                 ?>
+                <div class="collapsible"><?= $item[1] ?></div>
+                <div class="content">
+                    <input class="field" type="text" placeholder="Enter Amount" name="<?= $item[0] ?>_u" id="">
+                </div>
+                <?php endforeach ?>
+
+
+               
+
+                <button type="submit" name="addrequest" class="btn">ADD REQUEST</button>
 
 
             </form>
